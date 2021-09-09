71b61d613796fa4814823aa112c1d46c462f3e25
Daniella Nistorwod <nistorwood@ymail.com>
2021-09-09 12:28:49 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Daniella Nistorwood on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
